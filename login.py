0c358041d946689bbbb4fa6caf3145c1254a7e3e
Sivashree-117 <sivashree.pl2023@vitstudent.ac.in>
2025-12-23 20:52:05 +0530

    addd

---
+++ login.py
@@ -0,0 +1,17 @@
+#login.py
+def login(username, password):
+    # Predefined credentials (for demo purpose)
+    stored_username = "admin"
+    stored_password = "admin123"
+
+    if username == stored_username and password == stored_password:
+        return "Login successful"
+    else:
+        return "Invalid username or password"
+# Test the login feature
+if __name__ == "__main__":
+    user = input("Enter username: ")
+    pwd = input("Enter password: ")
+
+    result = login(user, pwd)
+    print(result)
